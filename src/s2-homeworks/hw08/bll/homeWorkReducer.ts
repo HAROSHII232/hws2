35f76a3e1c43ac01b0dd1380e217a9fee84be112
Ihar Voitau <igorgrime@gmail.com>
2024-08-05 14:31:17 +0300

    feat: solve HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,28 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  switch (action.type) {
+    case "sort": {
+      if (action.payload === "up") {
+        return state.sort((a, b) => a.name.localeCompare(b.name));
+      }
+      if (action.payload === "down") {
+        return state.sort((a, b) => b.name.localeCompare(a.name));
+      }
+      break
     }
-}
+    case "check": {
+      return state.sort((a, b) => a.name.localeCompare(b.name)).filter((u) => u.age >= 18);
+    }
+    default:
+      return state;
+  }
+  return state
+};
